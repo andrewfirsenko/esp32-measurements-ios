c23bdc42ab81b2fe2c555a1ebc7af2fe22393507
Andrew Firsenko <andrewfirsenko@gmail.com>
2026-01-04 20:20:21 +0300

    Init Xcode project

---
+++ ESP32Measurements/ESP32MeasurementsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ESP32MeasurementsApp.swift
+//  ESP32Measurements
+//
+//  Created by Andrew on 04.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct ESP32MeasurementsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
